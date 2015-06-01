ff6451181b00d33b1e10dc1a898588cb3b4424a3
Alexis Quintero <alexis_quintero@hotmail.com.ar>
2015-06-01 17:53:27 -0300

    TP2Lab02 terminado punto 1. Falta punto 2

---
+++ TP2L01/Business.Entities/ModuloUsuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Entities
+{
+    public class ModuloUsuario
+    {
+        private int IdUsuario;
+        private int IdModulo;
+        private bool PermiteAlta;
+        private bool PermiteBaja;
+        private bool PermiteModificacion;
+        private bool PermiteConsulta;
+    }
+}
---
+++ TP2L01/Business.Entities/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Entities
+{
+    public class Usuario
+    {
+        private string NombreUsuario;
+        private string Clave;
+        private string Nombre;
+        private string Apellido;
+        private string Email;
+        private bool Habilitado;
+    }
+}
