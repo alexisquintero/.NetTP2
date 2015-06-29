f5aabcee0a9c828812ecc037b37a06bdd604d548
PC-NET <crzemanuelc@gmail.com>
2015-06-29 19:11:53 -0300

    agregue comision.cs

---
+++ TP2L01/Business.Entities/Comision.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Entities
+{
+    class Comision: BusinessEntity
+    {
+        public int AnioEspecialidad { get; set; }
+        public int IDPlan { get; set; }
+        public string Descripcion { get; set; }
+        
+    }
+    
+    }
+
