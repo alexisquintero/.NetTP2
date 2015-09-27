0c295a31b59813d44bcc8768ff3f958f66c4c423
Alexis Quintero <alexisquintero@hotmail.com.ar>
2015-09-27 17:07:14 -0300

    TP2 Laboratorio 5
    No anda bien los 4 botones de la interfaz gráfica, actualizar, nuevo, editar y eliminar

--- Data.Database/Data.Database/Adapter.cs
+++ Data.Database/Data.Database/Adapter.cs
@@ -9,30 +9,27 @@ namespace Data.Database
 {
     public class Adapter
     {
-//        private SqlConnection sqlConnection = new SqlConnection("ConnectionString;");
+        const string consKeyDefaultCnnString = "ConnStringLocal"; //Para uso local
+//       const string consKeyDefaultCnnString = "ConnStringExpress"; //Para uso con con sql express
+//        const string consKeyDefaultCnnString = "ConnStringSereverISI"; //Para usar en clases
+        public SqlConnection sqlConn;
 
         protected void OpenConnection()
         {
-            String conn = ConfigurationManager.ConnectionStrings[consKeyDefaultCnnString].ConnectionString;           
+            String conn = ConfigurationManager.ConnectionStrings[consKeyDefaultCnnString].ConnectionString;  
             sqlConn = new SqlConnection(conn);
             sqlConn.Open();
-//            throw new Exception("Metodo no implementado");
         }
 
         protected void CloseConnection()
         {
             sqlConn.Close();
             sqlConn = null;
-//            throw new Exception("Metodo no implementado");
         }
 
         protected SqlDataReader ExecuteReader(String commandText)
         {
             throw new Exception("Metodo no implementado");
-        }
-
-        const string consKeyDefaultCnnString = "ConnStringLocal";
-
-        public SqlConnection sqlConn;
+        }       
     }
 }
